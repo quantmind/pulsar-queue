1d40559826acec3c691839233c3203cf320fb9c7
Luca Sbardella <luca@quantmind.com>
2016-07-05 09:13:51 +0100

    expose StatusType to the api

--- pq/api.py
+++ pq/api.py
@@ -2,6 +2,7 @@ from .server.apps import TaskApp, PulsarQueue
 from .mq import TaskFuture, MQ
 from .server import TaskScheduler, TaskConsumer
 from .tasks.task import TaskError, TaskNotAvailable, TaskTimeout, Task
+from .tasks.states import StatusType
 from .tasks.models import (job, Job, PeriodicJob, EventDriven, anchorDate,
                            ASYNC_IO, GREEN_IO, THREAD_IO, CPUBOUND)
 
@@ -12,6 +13,7 @@ __all__ = ['TaskApp',
            'TaskNotAvailable',
            'TaskTimeout',
            'Task',
+           'StatusType',
            'TaskFuture',
            'TaskScheduler',
            'TaskConsumer',

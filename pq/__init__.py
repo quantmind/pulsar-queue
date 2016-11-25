ca0f4967f6f818e8ee5ccb13ac0a26c020153f66
Luca Sbardella <luca@quantmind.com>
2016-11-25 08:08:51 +0000

    preparing release

--- pq/__init__.py
+++ pq/__init__.py
@@ -1,5 +1,5 @@
 """Asynchronous task queue"""
 from .utils.version import get_version
 
-VERSION = (0, 5, 1, 'alpha', 0)
+VERSION = (0, 5, 1, 'final', 0)
 __version__ = get_version(VERSION, __file__)
